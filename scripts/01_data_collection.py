d56344f6038abf109d2197051032b13b04d38f17
Rashid Ahmadi~ <rashid.angury20@gmail.com>
2026-08-11 08:59:34 +0200

    second version

--- scripts/01_data_collection.py
+++ scripts/01_data_collection.py
@@ -8,8 +8,50 @@ Created on Mon Aug 10 13:33:34 2026
 
 # %%
 
+
+"""
+German Stock Market EDA
+
+Project:
+Explorative Datenanalyse deutscher Unternehmen
+
+Purpose:
+- Load stock market data
+- Verify data structure
+- Preserve raw data
+- Prepare data for subsequent EDA
+"""
+
+from pathlib import Path
+
+
+import numpy as np
 import yfinance as yf
 import pandas as pd
+
+# %%
+
+# Project paths
+
+SCRIPT_DIR = Path(__file__).resolve().parent
+PROJECT_DIR = SCRIPT_DIR.parent
+
+RAW_DIR = PROJECT_DIR / "Data" / "raw"
+PROCESSED_DIR = PROJECT_DIR / "Data" / "processed"
+FIGURES_DIR = PROJECT_DIR / "figures"
+
+print("Project directory:")
+print(PROJECT_DIR)
+
+print("\nRaw data directory:")
+print(RAW_DIR)
+# %%
+
+"""
+The raw dataset contains daily historical market data for eight German companies 
+and the DAX benchmark. The data returned by yfinance is structured using a pandas MultiIndex, 
+with price variables as the first level and ticker symbols as the second level.
+"""
 
 tickers={
     "SAP": "SAP.DE",
@@ -34,16 +76,44 @@ data=yf.download(
 print(data.head())
 print(data.shape)
 # %%
+#Genrating csv file 
 
-print(data.columns)
-print(data.index)
+file_path=RAW_DIR/"german_stocks_raw.csv"
+data.to_csv(file_path)
+
 # %%
 
-print(data.dtypes)
-print(data.info())
+# loading data from CSV
+
+file_path = RAW_DIR / "german_stocks_raw.csv"
+
+df = pd.read_csv(
+    file_path,
+    index_col=0,
+    parse_dates=True
+)
+# %%
+
+#checking head and tail
+
+print(df.head())
+print(df.tail())
+# %%
+#check shape and columns
+
+print(df.shape)
+print(df.columns)
+
+# %%
+#check index, data type and print info
+
+print(df.index)
+print(df.dtypes)
+print(df.info())
 # %%
 
 #Check the time range
+
 print("Start:", data.index.min())
 print("End:", data.index.max())
 # %%
@@ -54,10 +124,12 @@ print("Number of trading days:", len(data))
 # %%
 
 #Check missing values
+
 print(data.isna().sum())
 # %%
 
 #heck the percentage too
+
 missing_pct = data.isna().mean() * 100
 
 print(missing_pct)
@@ -70,4 +142,3 @@ print("Duplicate dates:", data.index.duplicated().sum())
 print("Sorted:", data.index.is_monotonic_increasing)
 # %%
 
-data.to_csv("german_stocks_raw.csv")
